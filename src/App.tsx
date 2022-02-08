de43b08debc7a63142083e731e0ff5f1faedc77d
OutisNoMore <OutisNoMore@protonmail.com>
2022-02-07 21:54:42 -0800

    basic changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// put all necessary body elements onto page
+class Body extends React.Component{
+  render() {
+    return (
+      <main>
+        <p>Welcome to the Image Processing Toolkit</p>
+      </main>
+    );
+  }
+}
+
+// put all necessary header elements onto webpage
+class Header extends React.Component{
+  render() {
+    return (
+      <header className="head">
+        <h1>Image Processing Toolkit!</h1>
+      </header>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-page">
+      <div className="App-head">
+        <Header />
+      </div>
+      <div className="App-body">
+        <Body />
+      </div>
     </div>
   );
 }
