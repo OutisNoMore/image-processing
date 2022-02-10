5008e25d5a5dbe3ee56f76704e0ff708b4323c9c
OutisNoMore <OutisNoMore@protonmail.com>
2022-02-10 14:48:57 -0800

    Implement loading a file

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,16 @@ class ProcessImage extends React.Component{
     let selectBox = document.getElementById("toolkit") as HTMLSelectElement;
     let selectedValue:string = selectBox.options[selectBox.selectedIndex].value;
 
-    console.log(selectedValue);
+    if(selectedValue === "invert"){
+      this.invert();
+    } else if(selectedValue === "grayscale"){
+      this.grayscale();
+    } else if(selectedValue === "brightness"){
+      this.brightness(0.5);
+    } else{
+      alert("bad choice");
+    }
+
     selectBox.selectedIndex = 0;
   }
 
@@ -58,17 +67,36 @@ class ProcessFile extends React.Component{
   canvas: any;
   context: any;
 
+  getFile(event: any): void {
+    if(event.currentTarget.files && event.currentTarget.files[0]){
+      let reader = new FileReader();
+      reader.readAsDataURL(event.currentTarget.files[0]);
+
+      let img: HTMLImageElement = new Image() as HTMLImageElement;
+      reader.onload = (event: any) => {
+        if(event.currentTarget){
+          img.src = event.currentTarget.result;
+          img.onload = () => {
+            this.canvas.width = img.width;
+            this.canvas.height = img.height;
+            this.context.drawImage(img, 0, 0);
+
+          }
+        }
+      }
+    }
+  }
+
   open(): boolean {
-    this.canvas = document.getElementById("picture") as HTMLCanvasElement;
-    this.context = this.canvas.getContext("2d");
-    this.context.fillStyle = 'red';
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    const fileElem = document.getElementById("fileElem") as HTMLInputElement;
+    if(fileElem){
+      fileElem.click();
+    }
 
     return false;
   }
 
   download(): boolean {
-
     return false;
   }
 
@@ -78,28 +106,41 @@ class ProcessFile extends React.Component{
   }
 
   fileProcess(): void {
+    this.canvas = document.getElementById('picture') as HTMLCanvasElement;
+    if(this.canvas.getContext){
+      this.context = this.canvas.getContext('2d');
+    }
+
     let selectBox = document.getElementById("file") as HTMLSelectElement;
     let selectedValue:string = selectBox.options[selectBox.selectedIndex].value;
-
     console.log(selectedValue);
+    if(selectedValue === "open"){
+      this.open();
+    } else if(selectedValue === "download"){
+      this.download();
+    }
+
     selectBox.selectedIndex = 0;
   }
   
 
   render(){
     return(
-      <select id="file" onChange={() => this.fileProcess()}>
-        <option value="">--File--</option>
-        <option value="open">Open</option>
-        <option value="save">Download</option>
-        <option value="close">Close</option>
-      </select>
+      <div className="files">
+        <select id="file" onChange={() => this.fileProcess()}>
+          <option value="">--File--</option>
+          <option value="open">Open</option>
+          <option value="download">Download</option>
+          <option value="close">Close</option>
+        </select>
+        <input type="file" id="fileElem" accept="image/jpeg" onChange={(event: React.ChangeEvent<HTMLInputElement>) => this.getFile(event)}/>
+      </div>
     );
   }
 }
 
 // put all necessary body elements onto page
-class Body extends React.Component{
+class Select extends React.Component{
   render() {
     return (
       <div className="body">
@@ -144,9 +185,8 @@ function App() {
         <Header />
       </div>
       <div className="App-body">
-        <Body />
+        <Select />
         <canvas id="picture" width="600" height="600"></canvas>
-        <input type="file" id="fileElem" accept="image/jpeg" />
       </div>
     </div>
   );
